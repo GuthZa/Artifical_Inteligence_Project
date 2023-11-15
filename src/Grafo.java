5154a611e06f3e2d22a8c3576a8b49ba5e5bfea0
GuthZa <joao9.claro99@gmail.com>
2023-11-15 16:35:12 +0000

    trepa colinas a funcionar aleatoriamente

---
+++ src/Grafo.java
@@ -0,0 +1,158 @@
+import java.io.*;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Grafo {
+
+    //Matriz de adjacentes
+    int[][] matrix;
+    int vertices;
+    int interactions;
+    int[] solution;
+
+    public Grafo(File file) throws IOException {
+
+        fillData(file);
+    }
+
+    public int trepa_colinas() {
+        int cost, neighborCost;
+
+        //calculate the cost of the Initial Solution
+        cost = calculate_Cost(solution);
+        for (int i = 0; i < interactions; i++) {
+            //Create the neighbor
+            int[] new_Solution = create_Neighbors();
+            //Calculates the cost of the new neighbor
+            neighborCost = calculate_Cost(new_Solution);
+            //If the neighbor cost is lower than the initial cost, swap them (Minimization problem)
+            if (neighborCost < cost) {
+                this.solution = new_Solution;
+                cost = neighborCost;
+            }
+        }
+        return cost;
+    }
+
+    private int calculate_Cost(int[] sol) {
+        int total = 0;
+
+        for (int i = 0; i < vertices; i++) {
+            if (sol[i]==0)
+            {
+                for (int j = 0; j < vertices; j++) {
+                    if (sol[j]==1 && matrix[i][j]==1)
+                        total++;
+                }
+            }
+        }
+
+        return total;
+    }
+
+    private int[] create_Neighbors() {
+        int[] neighbor_Solution = new int[vertices];
+        Random random = new Random();
+        for (int i = 0; i < vertices; i++) {
+            neighbor_Solution[i] = solution[i];
+        }
+        int p1, p2;
+        // Find positions with value 0
+        do {
+            p1 = random.nextInt(vertices);
+        } while (neighbor_Solution[p1] != 0);
+
+        do {
+            p2 = random.nextInt(vertices);
+        } while (neighbor_Solution[p2] != 1);
+
+        //Switch
+        neighbor_Solution[p1] = 1;
+        neighbor_Solution[p2] = 0;
+
+        return neighbor_Solution;
+    }
+
+    private void fillData(File file) throws IOException {
+        Scanner scanner = new Scanner(file);
+
+        if (scanner.hasNextInt()) {
+            this.interactions = scanner.nextInt();
+            this.vertices = scanner.nextInt();
+
+            this.solution = new int[vertices];
+
+            this.matrix = new int[vertices][vertices];
+
+            for (int i = 0; i < vertices; i++) {
+                for (int j = 0; j < vertices; j++) {
+                    matrix[i][j] = scanner.nextInt();
+                }
+            }
+        } else {
+            System.out.println("Empty File!");
+        }
+    }
+
+    public void create_Start_Solution() {
+        Random random = new Random();
+        for (int i = 0; i < vertices; i++) {
+            solution[i] = 0;
+        }
+        for (int i = 0, x; i < vertices / 2; i++) {
+            do {
+                x = random.nextInt(vertices);
+            } while (solution[x] != 0);
+            solution[x] = 1;
+        }
+    }
+
+    public int[] getSolution() {
+        return solution;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public int getVertices() {
+        return vertices;
+    }
+
+    private void setVertices(int vertices) {
+        this.vertices = vertices;
+    }
+
+    public int getInteractions() {
+        return interactions;
+    }
+
+    private void setInteractions(int interactions) {
+        this.interactions = interactions;
+    }
+
+    public void printMatrix(){
+        //For debugging, to have a visualisation
+         for (int i = 0; i < vertices; i++) {
+             for (int j = 0; j < vertices; j++) {
+                 System.out.print(matrix[i][j]);
+             }
+             System.out.println();
+         }
+    }
+
+    public void printSolution(int[] solution) {
+        System.out.print("0s: ");
+        for (int i = 0; i < vertices; i++) {
+            if (solution[i]==0)
+                System.out.print(i + " ");
+        }
+        System.out.print("\n1s: ");
+        for (int i = 0; i < vertices; i++) {
+            if (solution[i]==1)
+                System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+}
---
+++ src/main.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class main {
+
+    private static final int DEFAULT_RUNS = 10;
+    public static void main(String[] args) throws IOException {
+
+        String file_name;
+        int runs, custo, melhor_custo = 0;
+        double mbf = 0.0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Runs: ");
+        runs = scanner.nextInt();
+        System.out.println("File Name: ");
+        file_name = scanner.next();
+
+        File file = new File(file_name);
+        if (!file.exists()) {
+            System.out.println("Error opening the file");
+            return;
+        }
+        if (runs <=0) {
+            System.out.println("Num of runs needs to be higher than 0");
+            return;
+        }
+
+        Grafo grafo = new Grafo(file);
+
+        int i = 0;
+        int[] best_solution = new int[grafo.getVertices()];
+        for (; i < runs; i++) {
+            System.out.println("Initial: ");
+            grafo.create_Start_Solution();
+            grafo.printSolution(grafo.getSolution());
+
+            custo = grafo.trepa_colinas();
+            System.out.println("\nRep: " + i);
+            grafo.printSolution(grafo.getSolution());
+            System.out.println("Final cost: " + custo);
+            mbf += custo;
+            if (i == 0 || melhor_custo > custo) {
+                melhor_custo = custo;
+                best_solution = grafo.getSolution();
+            }
+            //For better readability
+            System.out.println();
+        }
+
+        System.out.println("\n\nMBF: " + mbf/i);
+        System.out.println("Best solution found: ");
+        grafo.printSolution(best_solution);
+        System.out.println("Final cost: " + melhor_custo);
+    }
+}
