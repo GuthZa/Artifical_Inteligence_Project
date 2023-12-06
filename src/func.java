53ea10718f802ee520aa94f6b7367ba40f8f755b
GuthZa <joao9.claro99@gmail.com>
2023-12-06 14:23:31 +0000

    now discard all solutions that are invalid, meaning, that have AT LEAST 1 point that has no connections

--- src/func.java
+++ src/func.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class func {
 
@@ -24,26 +25,38 @@ public class func {
         //METODO HIBRIDO
         //2 ABORDAGENS HIBRIDAS USANDO OS 2 ALGORITMOS
 
+
+
+
+        /*NOTAS:
+            - Como saber que um dos pontos nao tem vertices na solucao?
+             - ex: [110011] - o ponto 6 so tem ligacao com o 4, mas o 4 nao e ponto 1 na solucao
+             - logo isto gera uma solucao invalida
+
+         */
+
+        int[] copy = new int[vertices];
+
         for (Edge edge : edgeList) {
             if (solution[edge.getStart() - 1] == 1 && solution[edge.getEnd() - 1] == 1) {
                 total += edge.getCost();
+                //Make the positions that have valid connections 1
+                //The idea is that by the end of all edges, you would have a
+                //copy equal to the original solution
+                //This would mean that all points in the original had connections and therefore costs
+                //By analysing which points in the copy are 0 and 1 in the original,
+                // you can get those that don't have connections
+                //Making the solution invalid
+                copy[edge.getStart() - 1] = 1;
+                copy[edge.getEnd() - 1] = 1;
             }
         }
-
-//        for (int i = 0; i < vertices; i++) {
-//            if (solution[i] == 1) {
-//                for (int j =  i + 1; j < vertices; j++) {
-//                    if (solution[j] == 1) {
-//                        for (Edge edge : edgeList) {
-//                            if (edge.getStart() == (i+1) && edge.getEnd() == (j+1) ||
-//                            edge.getEnd() == (i+1) && edge.getStart() == (j+1)) {
-//                                total += edge.getCost();
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        }
+        //If the copy is equal to the original, then the solution is valid
+        //Otherwise, one of the points did not have connections to other points in the solution
+        if (!Arrays.equals(copy, solution)) {
+            //Invalid Solution
+            return 0;
+        }
 
         return total;
     }
